8429c4eaa53f2fe360e61d1489323d43176b06ee
Gabriel <gvogt@asu.edu>
2020-04-18 13:23:50 -0700

    Add project files.

---
+++ VogtFactory2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VogtFactory2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
